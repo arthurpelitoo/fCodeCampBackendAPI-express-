babce41b86a2b45dcccfca83a192afcf783f10f3
arthurpelitoo <arthur.pelito@escola.pr.gov.br>
2024-12-08 14:38:22 -0300

    first commit

---
+++ myApp.js
@@ -0,0 +1,42 @@
+let express = require('express');
+let app = express();
+
+// let HelloWorld = "Hello World";
+
+console.log("Hello World");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
